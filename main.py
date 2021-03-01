eca52031f0d44f50f369cd1f3dd95b84349f611d
Maciej Laskowski <maciej.laskowski@yahoo.com>
2021-03-01 04:38:49 +0000

    find an odd int

--- main.py
+++ main.py
@@ -27,7 +27,40 @@ def split_to_pair_of_chars(text):
 
 split_to_pair_of_chars('abcdefghijk')
 
+'''
+Given an array of integers, find the one that appears an odd number of times.
 
+There will always be only one integer that appears an odd number of times.
+'''
+
+
+def find_it(seq):
+
+    numbers = [x for x in seq if seq.count(x) % 2]
+    i = 0
+
+    while i < len(numbers) and len(numbers) > 1:
+        numbers.pop()
+        i += 1
+
+
+def find_it1(seq):
+
+    for x in seq:
+        if seq.count(x) % 2:
+            return x
+
+def find_it3(seq):
+    return [x for x in seq if seq.count(x) % 2][0]
+
+
+find_it([2, 2, 2, 2, 4, 4, 6, 6, 6])
+print(find_it3([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
+find_it1([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5])
+find_it1([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5])
+find_it1([10])
+find_it1([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1])
+find_it1([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10])
 
 
 
