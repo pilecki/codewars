4842b21451ab4a30085bb202461496463e14da1b
Maciej Laskowski <maciej.laskowski@yahoo.com>
2021-03-01 01:33:27 +0000

    split_to_pair_of_chars

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+'''
+Complete the solution so that it splits the string into pairs of two characters.
+If the string contains an odd number of characters then it should replace
+the missing second character of the final pair with an underscore ('_').
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Examples:
+
+solution('abc') # should return ['ab', 'c_']
+solution('abcdef') # should return ['ab', 'cd', 'ef']
+'''
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def split_to_pair_of_chars(text):
+
+    text1 = [x for x in text]
+    new_text = []
+    i = 0
+    while i <= len(text1) - 1:
+        if i + 1 == len(text1):
+            new_text.append(text1[i] + '_')
+            break
+        new_text.append(text1[i] + text1[i + 1])
+
+        i += 2
+    return new_text
 
 
-# Press the green button in the gutter to run the script.
+split_to_pair_of_chars('abcdefghijk')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
